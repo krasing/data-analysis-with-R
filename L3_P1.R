cb857a2849af405bb152e64b79bb3b799127f508
krasing <krasing@users.noreply.github.com>
2015-08-18 18:05:32 +0100

    Update L3_P1.R

--- L3_P1.R
+++ L3_P1.R
@@ -146,3 +146,35 @@ qplot(x = price, data = diamonds) + facet_wrap(~cut, scales="free_y")
 
 # ENTER YOUR CODE BELOW THIS LINE.
 # ===========================================================================
+
+
+
+
+# ===========================================================================
+summary(diamonds$carat)
+
+qplot(data = diamonds, x = carat, geom = 'freqpoly')
+
+qplot(data = diamonds, x = carat, geom = 'freqpoly', binwidth = 0.1) +
+scale_x_continuous(limits = c(0, 3), breaks = seq(0, 3, 0.2))
+
+qplot(data = diamonds, x = carat, geom = 'freqpoly', binwidth = 0.001) +
+scale_x_continuous(limits = c(0.9, 1.1))
+
+# The Gapminder website contains over 500 data sets with information about
+# the world's population. Your task is to download a data set of your choice
+# and create 2-5 plots that make use of the techniques from Lesson 3.
+
+# You might use a simple histogram, a boxplot split over a categorical variable,
+# or a frequency polygon. The choice is yours!
+
+# You can find a link to the Gapminder website in the Instructor Notes.
+
+# Once you've completed your investigation, create a post in the discussions that includes:
+#       1. any questions you answered, your observations, and summary statistics
+#       2. snippets of code that created the plots
+#       3. links to the images of your plots
+
+# Copy and paste all of the code that you used for
+# your investigation, and submit it when you are ready.
+# ====================================================================================
